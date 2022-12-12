4a85d8d430daca7916e548c10aa0a3291e6d8e54
Vinogradov Anton Leonidovich <antvinogradov@ozon.ru>
2022-12-12 20:01:48 +0300

    added num searcger

--- massIncreaser.js
+++ massIncreaser.js
@@ -1,10 +1,13 @@
 const initialMass = [-5, 0, 1, 3, 8, 12]
 let increasedMass = []
 
-let massIncreaser = (mass, increasedMass) => {
+const massIncreaser = (mass, increasedMass) => {
     mass.forEach(el => {
         increasedMass.push(el*el)
+        console.log('element ' , el)
+        console.log('mass ' , increasedMass)
     })
+    console.log('unsrt mass ' , increasedMass)
     return(increasedMass.sort((a, b) => a - b))
 }
 
--- numSearcher.js
+++ numSearcher.js
@@ -1,40 +1,23 @@
-const initialString = 'b3kjh12bh1223kjh1123123124b124kh1b4kh1b34kh123bkh4b1123123124123123'
+const initialString = 'basdasdsa3kjh12bh1223kjh1123123124b124kh1b4kh1b34kh123bkh4b1123123124123123dasda dasd as da1231  sa'
 let highestNum = null
 
 let numSearcher = (string, maxNum) => {
     let currentNum = ''
     string.split('').forEach(el => {
-        console.log(maxNum)
-        console.log(currentNum)
 
         if(el.search(/\d/) === -1 ) {
-            if (maxNum === null && currentNum === '') {
-                console.log('step 0')
-            }else if (maxNum === null) {
-                console.log('step 1')
-                maxNum = Number(currentNum)
                 currentNum = ''
-            } else if (maxNum < Number(currentNum)) {
-                console.log('step 2')
-                maxNum = Number(currentNum),
-                currentNum = ''
-            } else {
-                console.log('step 3')
-                currentNum = ''
-            } 
-        } else {    
-            console.log('step 4')
+            }
+        else {    
             currentNum = currentNum+el
+            if (maxNum === null) {
+                maxNum = Number(currentNum)
+            } else if (maxNum < Number(currentNum)) {
+                maxNum = Number(currentNum)
+            }
         }
-        
-        
     })
-    if (maxNum < Number(currentNum)) {
-        maxNum = Number(currentNum)
-        currentNum = ''
-        return (maxNum)
-    }
-    
+    return (maxNum)    
 }
 
 console.log(numSearcher(initialString, highestNum))
